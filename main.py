c9856810f944928d4041ae80c6d4b5ca1b0e3914
Miguel <maicolguap90@gmail.com>
2020-09-23 17:20:27 -0500

    se implementa la persistencia de datos

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import argparse
+import csv
+import datetime
 import logging
+from requests.exceptions import HTTPError
+from urllib3.exceptions import MaxRetryError
+import re
+
+is_well_formed_link = re.compile(r'^https?://.+/.+$')
+is_root_path = re.compile(r'^/.+$')
+
 logging.basicConfig(level=logging.INFO)
 
+import news_page_objects as news
 from common import config
+
 
 logger = logging.getLogger(__name__)
 
@@ -10,6 +21,33 @@ def _news_scraper(news_site_uid):
     host = config()['news_sites'][news_site_uid]['url']
 
     logging.info(f'Beginning scraper for {host}')
+    homepage = news.HomePage(news_site_uid, host)
+
+    articles =[]
+    for link in homepage.article_links:
+
+        article = _fetch_article(news_site_uid, host, link)
+        
+        if article:
+            logger.info('Article fetched!!')
+            articles.append(article)
+            #print(article.title)
+
+    #print(len(articles))
+    _save_articles(news_site_uid, articles)
+
+def _fetch_article(news_site_uid, host, link):
+    logger.info(f'Start fetching article at {link}')
+
+    article = None
+
+    try:
+        article = news.ArticlePage(news_site_uid, _build_link(host, link))
+    except (HTTPError, MaxRetryError) as e:
+        logger.warning('Invalid article. There is no body')
+        return None
+
+    return article
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
@@ -22,4 +60,26 @@ if __name__=="__main__":
 
     args = parser.parse_args()
     _news_scraper(args.news_sites)
-   
+
+def _build_link(host, link):
+    if is_well_formed_link.match(link):
+        return link
+    elif is_root_path.match(link):
+        return f'{host}{link}'
+    else:
+        return f'{host}/{link}'
+
+def _save_articles(news_site_uid, articles):
+    now = datetime.datetime.now().strftime('%Y_%m_%d')
+    out_file_name = f'{news_site_uid}_{now}_articles.csv'
+
+    csv_headers = list(filter(lambda property: not property.startswith(''), dir(articles[0])))
+
+    with open(out_file_name, mode='w+', encoding= 'utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerow(csv_headers)
+
+        for article in articles:
+            row = (str(getattr(article, prop)) for prop in csv_headers)
+            write.writerow(row)
+
--- news_page_objects.py
+++ news_page_objects.py
@@ -3,7 +3,7 @@ import requests
 
 from common import config
 
-class HomePage:
+class NewsPage:
 
     def __init__(self, news_site_uid, url):
         self._config = config()['news_site'][news_site_uid]
@@ -12,6 +12,9 @@ class HomePage:
 
         self._visit(url)
         
+    def _select(self, query_string):
+        return self._html.select(query_string)
+
 
     def _visit(self, url):
         response = requests.get(url)
@@ -19,6 +22,11 @@ class HomePage:
         response.raise_for_status()
 
         self._html = bs4.BeatifulSoup(response.text, 'html.parser')
+
+class HomePage(NewsPage):
+
+    def __init__(self, news_site_uid, url):
+        super().__init__(news_site_uid, url)
 
     @property
     def article_links(self):
@@ -29,7 +37,21 @@ class HomePage:
 
         return set(link['href']for link in link_list)
 
-    def _select(self, query_string):
-        return self._html.select(query_string)
+class ArticlePage(NewsPage):
+
+    def __init__(self, news_site_uid, url):
+        super().__init__(news_site_uid, url)
+
+    @property
+    def body(self):
+        result = self._select(self._queries['article_body'])
+        return result[0].text if len(result) else ''
+
+    @property
+    def title(self):
+        result = self._select(self._queries['article_title'])
+        return result[0].text if len(result) else ''
+
+    
         
 
