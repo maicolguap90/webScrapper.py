1514b6cc92d241a91aadaaec6e6da5831755d9b1
Miguel <maicolguap90@gmail.com>
2020-10-16 12:36:24 -0500

    Se impleneta los ultimos terminos para entregafinal

--- newspaper.py
+++ newspaper.py
@@ -18,6 +18,9 @@ def main(filename):
     df = _fill_missing_titles(df)
     df = _generate_uids_for_rows(df)
     df = _remove_new_lines_from_body(df)
+    df = _remote_duplicate_entries(df, 'title')
+    df = _drop_rows_with_missing_values(df)
+    _save_data(df, filename)
 
     return df
 
@@ -80,6 +83,23 @@ def _remove_new_lines_from_body(df):
     df['body'] = stripped_body
 
     return df
+
+
+def _remote_duplicate_entries(df, column_name):
+    logger.info('Removing duplicate entries')
+    df.drop_duplicates(subset=[column_name], keep='first', inplace=True)
+
+    return df
+
+
+def _drop_rows_with_missing_values(df):
+    logger.info('Dropping rows with missing values')
+    return df.dropna()
+
+def _save_data(df, filename):
+    clean_filename = 'clean_{}'.format(filename) 
+    logger.info('Saving data at location: {}'.format(clean_filename))
+    df.to_csv(clean_filename)
 
 
 if __name__=='__main__':
